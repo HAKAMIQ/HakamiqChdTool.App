b00c9cb9fae1669fccaae9d40f85bdb6119350ba
HAKAMIQ <almanestra@gmail.com>
2026-07-02 16:08:46 +0300

    Remove unused PS3 content intake island

--- Models/PlayStation/PS3ConversionPlan.cs
+++
@@ -1,6 +0,0 @@
-namespace HakamiqChdTool.App.Models.PlayStation;
-
-public sealed record PS3ConversionPlan(
-    bool CanProceed,
-    string Pipeline,
-    bool RequiresTemporaryIso);
--- Models/PlayStation/PS3PkgMetadata.cs
+++
@@ -1,8 +0,0 @@
-namespace HakamiqChdTool.App.Models.PlayStation;
-
-public sealed record PS3PkgMetadata(
-    bool IsValidPackage,
-    string? ContentId,
-    string? TitleId,
-    PS3ContentKind ContentKind,
-    bool IsProbablyEncrypted);
